69dab68eadee01dca9643e8baaddd960e4881e10
stephan <stephan@syntaktischer-zucker.de>
2020-09-04 15:54:17 +0200

    Updating deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,6 @@
 
 wget https://services.gradle.org/distributions/gradle-6.6.1-bin.zip
 unzip gradle-6.6.1-bin.zip
+ls -l
 ./gradle-6.6.1/bin/gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN bintrayUpload
+ls -l
