141c7acc2e83af9395e7668f09477990c1bba3bc
stephan <stephan@syntaktischer-zucker.de>
2020-09-04 15:27:38 +0200

    Update to Kotlin 1.4.0
    Install Gradle 6.6.1
    
    Fix for Gradle.
    
    Fix deployment.
    
    Finally fix deployment.
    
    Updating deploy script.
    
    Final fix for bintray.
    
    Adding Sha1 to version string.
    
    Cleanup.
    
    Update README.md
    
    Update README.md

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
-gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN jar
-gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN bundle
-gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN bintrayUpload
+#!/bin/bash
+
+wget https://services.gradle.org/distributions/gradle-6.6.1-bin.zip
+unzip gradle-6.6.1-bin.zip
+ls -l
+./gradle-6.6.1/bin/gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN -Psha=$(git rev-parse HEAD) jar
+./gradle-6.6.1/bin/gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN -Psha=$(git rev-parse HEAD) bundle
+./gradle-6.6.1/bin/gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN -Psha=$(git rev-parse HEAD) bintrayUpload
+ls -l
