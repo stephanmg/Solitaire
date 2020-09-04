79ac03856b6d9c8d29cdae97357a627dd57a6f62
stephan <stephan@syntaktischer-zucker.de>
2020-09-04 15:46:09 +0200

    Finally fix deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+wget https://services.gradle.org/distributions/gradle-6.6.1-bin.zip
+unzip gradle-6.6.1-bin.zip
 ./gradle-6.6.1/bin/gradle -Puser=stephanmg -Pkey=$BINTRAY_API_TOKEN bintrayUpload
