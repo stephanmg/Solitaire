6f1e424b9f3f3c8ccd66713f46549161c1b4daf4
stephan <stephan@syntaktischer-zucker.de>
2021-01-18 12:49:23 +0100

    Adding board configurator.

--- src/GUI.kt
+++ src/GUI.kt
@@ -13,6 +13,8 @@ import javafx.stage.Popup
 import javafx.stage.Stage
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 /**
  * @brief GUI
@@ -25,9 +27,9 @@ class GUI : Application() {
     private val pegImage = "file:peg.png"
     private fun createPegImage() = ImageView(Image(pegImage))
     private val n = 5
-    private val sizeX = 250.0*2
+    /// TODO: Improve alignment
+    private val sizeX = 250.0*2+100
     private val sizeY = 260.0*2
-    private var initialDraw = true
 
     /**
      * @brief start the stage
@@ -36,14 +38,15 @@ class GUI : Application() {
     override fun start(primaryStage: Stage) {
         val root = BorderPane()
         val scene = Scene(root, sizeX, sizeY)
-        val gridPane = GridPane()
+        var gridPane = GridPane()
         var curBtn: Button? = null
         var nextBtn: Button?
         var count = 0
-        // val currentBoard: Board = BoardFactory().square(n)  
-        val currentBoard: Board = BoardFactory().classic() 
+        var currentBoard: Board = BoardFactory().classic() 
         var fromPosX: Int = -1
         var fromPosY: Int = -1
+        val moveLabel = Label("Number of validMoves: 0")
+        var validMoves = 0
 
         /**
          * @brief callback to check and move pegs
@@ -55,9 +58,8 @@ class GUI : Application() {
                 nextBtn = null
                 fromPosX = i
                 fromPosY = j
-                btn.style = "-fx-background-color: beige; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
             }  else {
-                //canStillWin = Game().solveDfs(currentBoard)
+                // canStillWin = Game().solveDfs(currentBoard)
                 println("Can still win?" + canStillWin)
                 val jumpToX = fromPosX - i
                 val jumpToY: Int = fromPosY - j
@@ -82,6 +84,7 @@ class GUI : Application() {
                     nextBtn!!.graphic = createPegImage()
                     curBtn!!.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
                     nextBtn!!.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
+                    validMoves++
                 } else {
                     curBtn!!.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
                     nextBtn!!.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
@@ -90,6 +93,12 @@ class GUI : Application() {
             } 
             count++
 
+            /// move label
+            moveLabel.minWidth = 80.0
+            moveLabel.minHeight = 50.0
+            moveLabel.text = "Number of moves: ${validMoves}"
+
+            /// indicate loss or win
             val won: Boolean = currentBoard.numPegs() == 5
             val lost: Boolean = GameUtils.checkGameOver(currentBoard)
             if (won || lost) {
@@ -108,31 +117,47 @@ class GUI : Application() {
             }
         }
 
-        /* 
-        for (i in 0 until n) {
-            for (j in 0 until n) {
-                var btnBoard = Button()
-                if (!(i == ((n-1) / 2)  && j == ((n-1) / 2))) {
-                    btnBoard.graphic = createPegImage()
-                }
+        /**
+         * @brief configure board's view
+         */
+        val configureBoardView = fun() {
+            gridPane = GridPane()
+            drawBoard(gridPane, currentBoard, callback)
+            root.center=gridPane
+            gridPane.style = "-fx-grid-lines-visible: true;"
+        }
 
-                btnBoard.setMaxSize(50.0, 50.0)
-                btnBoard.setMinSize(50.0, 50.0)
-                btnBoard.onAction = EventHandler {
-                    callback(btnBoard, j, i)
-                }
+        /**
+         * @brief create board
+         */
+        val createBoard = fun(text: String) {
+            /// TODO: Refactor: use BoardFactory().board()
+            when (text) {
+                "Classic" -> currentBoard = BoardFactory().classic() 
+                "Square" -> currentBoard = BoardFactory().square(n)
+            } 
 
-                gridPane.add(btnBoard, j, i, 1, 1)
+            /// configure board
+            configureBoardView()
+        }
+
+        // Create menu selection
+        val menuButton = MenuButton("Board selector")
+        menuButton.getItems().addAll(MenuItem("Classic"), MenuItem("Square"))
+        menuButton.getItems().forEach {
+            val text = it.text
+            it.onAction = EventHandler() {
+                createBoard(text)
             }
         }
-        */
 
-        drawBoard(gridPane, currentBoard, callback, initialDraw)
-        initialDraw = false
+        drawBoard(gridPane, currentBoard, callback)
 
         root.center = gridPane
         root.top = Label(if (canStillWin) "Can still win" else "Cannot win anymore")
+        root.bottom = moveLabel
         gridPane.requestFocus()
+        root.right = menuButton
         /*gridPane.setMaxSize(sizeX, sizeY)
         gridPane.setMinSize(sizeX, sizeY)
         gridPane.setPrefSize(sizeX, sizeY)
@@ -164,7 +189,7 @@ class GUI : Application() {
      * @param gridPane
      * @param callback - button callback to remove and add pegs to the gridPane
      */
-    private fun drawBoard(gridPane: GridPane, board: Board, callback: (btn: Button, i: Int, j: Int) -> Unit, initialDraw: Boolean) {
+    private fun drawBoard(gridPane: GridPane, board: Board, callback: (btn: Button, i: Int, j: Int) -> Unit) {
         val draw = fun(i: Int, j: Int, value: Int) {
             var btn = Button()
             /// only non-empty holes get a peg
@@ -186,13 +211,11 @@ class GUI : Application() {
                 }
             }
 
-            if (initialDraw) {
-               if (value == -1) 
-                   btn.style = "-fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey; -fx-background-color: lightgrey"
-               else
-                   btn.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
+            if (value == -1) 
+                btn.style = "-fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey; -fx-background-color: lightgrey"
+            else
+                btn.style = "-fx-background-color: #f8f8ff; -fx-border-style: solid solid none solid; -fx-border-width: 1; -fx-border-color: grey"
 
-            }
 
             gridPane.add(btn, j, i, 1, 1)
         }
